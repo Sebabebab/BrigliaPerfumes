1760b7991fec52d7f4eefb560220c8e772299bbb
Sebabebab <122082924+Sebabebab@users.noreply.github.com>
2024-03-13 10:54:35 +0100

    har fikset mere på user button

--- src/components/User.js
+++ src/components/User.js
@@ -2,37 +2,40 @@ import React, { useState } from 'react';
 import './User.css';
 
 const User = () => {
-  const [showCloseIcon, setShowCloseIcon] = useState(false);
+  const [showSquare, setShowSquare] = useState(false);
 
-  const toggleCloseIcon = () => {
-    setShowCloseIcon(!showCloseIcon);
+  const toggleSquare = () => {
+    setShowSquare(!showSquare);
   };
 
   return (
-    <div className="user-icon" onClick={toggleCloseIcon}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="auto"
-        height="30"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="#DFC385"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={`feather ${showCloseIcon ? 'rotate180' : ''}`}
-      >
-        {!showCloseIcon && (
-          <path d="M12 2c2.76 0 5 2.24 5 5s-2.24 5-5 5-5-2.24-5-5 2.24-5 5-5zm0 10c2.67 0 8 1.34 8 4v2H4v-2c0-2.66 5.33-4 8-4z"></path>
-        )}
-        {showCloseIcon && (
-          <g>
-            <line x1="18" y1="6" x2="6" y2="18"></line>
-            <line x1="6" y1="6" x2="18" y2="18"></line>
-          </g>
-        )}
-      </svg>
-    </div>
+    <>
+      <div className="user-icon" onClick={toggleSquare}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="auto"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#DFC385" 
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className={`feather ${showSquare ? 'rotate180' : ''}`}
+        >
+          {!showSquare && (
+            <path d="M12 2c2.76 0 5 2.24 5 5s-2.24 5-5 5-5-2.24-5-5 2.24-5 5-5zm0 10c2.67 0 8 1.34 8 4v2H4v-2c0-2.66 5.33-4 8-4z"></path>
+          )}
+          {showSquare && (
+            <g>
+              <line x1="18" y1="6" x2="6" y2="18" stroke="#ffffff"></line>
+              <line x1="6" y1="6" x2="18" y2="18" stroke="#ffffff"></line> 
+            </g>
+          )}
+        </svg>
+      </div>
+      {showSquare && <div className="full-screen-square" onClick={toggleSquare}></div>}
+    </>
   );
 }
 
