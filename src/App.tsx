d5d58973410d3d05a31536622c11d943b9d030e8
Sebabebab <122082924+Sebabebab@users.noreply.github.com>
2024-06-06 12:19:21 +0200

    har importert riktige filer fra annet porsjekt sted

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,10 @@ import Pouring from './Bilder/pouring.jpg';
 import PouringPhone from './Bilder/pouringphone.jpg';
 import PouringIpad from './Bilder/pouringipad.jpg';
 import TopBar from './components/TopBar';
-<<<<<<< Updated upstream
-=======
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Login} from './pages/login';
 import { Home } from './pages/home';
 import Forms from './components/Forms';
->>>>>>> Stashed changes
 
 function App() {
   const handleSearch = (searchTerm: any) => {
@@ -20,6 +17,12 @@ function App() {
   return (
     <div className="App">
       <TopBar handleSearch={handleSearch} />
+      <HashRouter>
+          <Routes>
+          <Route path="/" element={<Home/>}/> 
+            <Route path="/login" element={<Login/>}/>
+          </Routes>
+        </HashRouter>
       <img src={Pouring} alt="Frontpage" className="pouring" />
       <img src={PouringPhone} alt="Frontpage" className="pouringphone" />
       <img src={PouringIpad} alt="Frontpage" className="pouringipad" />
--- src/components/UserIcon.js
+++ src/components/UserIcon.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import './UserIcon.css';
-import { Link } from 'react-router-dom';
-
 
 const UserIcon = ({ width, height, color }) => {
   const [isSpinning, setIsSpinning] = useState(false);
@@ -10,27 +8,27 @@ const UserIcon = ({ width, height, color }) => {
     setIsSpinning(true);
     setTimeout(() => {
       setIsSpinning(false);
+      window.location.hash = "/login"; 
     }, 500); 
   };
 
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="30"
+      width={width || "30"}
       height={height || "30"}
       viewBox="0 0 24 24"
       fill="none"
-      stroke="#DFC385"
+      stroke={color || "#DFC385"}
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
       className={`user-icon ${isSpinning ? 'spin-animation' : ''}`}
       onClick={handleClick}
     >
-      <circle cx="11" cy="11" r="8"/>
-      <rect x="1px" y="23px" width="20" height="10" rx="7" ry="7"/>
+      <circle cx="11" cy="11" r="8" />
+      <rect x="1" y="23" width="20" height="10" rx="7" ry="7" />
     </svg>
-    
   );
 };
 
